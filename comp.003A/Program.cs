73bc9f0730be4889c2acc6f96c46b618d55a8e03
romanvillagran <romanvillagran386@gmail.com>
2025-01-15 20:36:36 -0800

    Add project files.

---
+++ comp.003A/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace comp._003A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
