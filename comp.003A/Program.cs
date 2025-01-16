766713b6dc8885eb594b9d53e7fd8dc7996927d4
romanvillagran <romanvillagran386@gmail.com>
2025-01-15 20:50:04 -0800

    changed some stuff around

--- comp.003A/Program.cs
+++ comp.003A/Program.cs
@@ -5,19 +5,11 @@
         static void Main(string[] args)
         {
             //prompts user for input
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hi, my name is Roman Villagran.");
-            Console.WriteLine("1");
-            Console.WriteLine("2");
-            Console.WriteLine("3");
-            Console.WriteLine("4");
-            Console.WriteLine("5");
-            Console.WriteLine("6");
-            Console.WriteLine("7");
-            Console.WriteLine("8");
-            Console.WriteLine("9");
-            Console.WriteLine("10");
-
+            Console.Write("Enter your name: ");
+            string username = Console.ReadLine();// getd the user input and save it to 
+            
+            Console.WriteLine("HI " + username + ". My name is Roman");
+            
         }
     }
 }
