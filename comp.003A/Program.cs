88c85db622220aa9e344737b7d211d43c860ee39
Your Name <you@example.com>
2025-01-16 22:04:47 -0800

    added the 10 things about me

--- comp.003A/Program.cs
+++ comp.003A/Program.cs
@@ -6,9 +6,22 @@
         {
             //prompts user for input
             Console.Write("Enter your name: ");
-            string username = Console.ReadLine();// getd the user input and save it to 
+            string username = Console.ReadLine();// gets the user input and save it to 
             
             Console.WriteLine("HI " + username + ". My name is Roman");
+
+
+            Console.WriteLine("Here are 10 things about me:");
+            Console.WriteLine("1.I love video games");
+            Console.WriteLine("2.I love history");
+            Console.WriteLine("3.I like Chinese food");
+            Console.WriteLine("4.I will listen to every type of music but country");
+            Console.WriteLine("5.I like food");
+            Console.WriteLine("6.My favorite video game is Monster Hunter World:Iceborne");
+            Console.WriteLine("7.My current favorite show is Arcane");
+            Console.WriteLine("8.I'm not a fan of pickles");
+            Console.WriteLine("9.I don't like olives");
+            Console.WriteLine("10.I am bilingual");
             
         }
     }
